eb2c85d0ec35f3e8b970eb1723bba56c79b355b9
Combatd <combatdstar@gmail.com>
2020-10-27 08:03:23 -0700

    Import starter for ChaosGame

---
+++ SimpleCPlusPlus/ChaosGame/ChaosGame/main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  ChaosGame
+//
+//  Created by Mark Calvelo on 10/27/20.
+//
+
+/*
+ For this problem, you will use the graphics library to explore an interesting phenomenon known as
+ the "Chaos Game."
+ Prompt the user to click three points within the graphics window. We'll refer to these points as A, B,
+ C. Draw a triangle connecting A, B, and C. Now, follow these steps to fill in the triangle with points:
+ 1. Randomly choose one vertex (A, B, or C) as the current point
+ 2. Draw a small filled circle around the current point
+ 3. Randomly choose one vertex (A, B, or C) and move the current point half of the distance
+ toward that vertex
+ 4. Repeat steps 2 & 3 (stop when the user clicks the mouse)
+ After many iterations, you might expect this random process to produce nothing but an odd
+ mishmash or that the points will eventually fill the entire triangle. Without giving too much away, I'll
+ tell you that the resulting image never varies and will likely surprise you with its beauty and
+ symmetry.
+ */
+
+#include <iostream>
+#include "lib/graphics.h"
+#include "lib/extgraph.h"
+
+int main() {
+    return 0;
+}
