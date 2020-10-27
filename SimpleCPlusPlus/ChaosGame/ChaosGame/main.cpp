3aa049e6b582816c1e35e69cb4cf19d791fc1107
Combatd <combatdstar@gmail.com>
2020-10-27 08:12:44 -0700

    Declare firstVertex, secondVertex, thirdVertex from struct Point

--- SimpleCPlusPlus/ChaosGame/ChaosGame/main.cpp
+++ SimpleCPlusPlus/ChaosGame/ChaosGame/main.cpp
@@ -19,11 +19,40 @@
  mishmash or that the points will eventually fill the entire triangle. Without giving too much away, I'll
  tell you that the resulting image never varies and will likely surprise you with its beauty and
  symmetry.
+ 
+ CS106B uses a simple paint-based drawing library that operates on a Cartesian coordinate system
+ where dimensions are expressed in inches. The graphics.h and extgraph.h header files give
+ details on the individual functions. For this program, you'll use the functions for handling mouse
+ events (WaitForMouseDown, GetMouseX, etc.) and pen-drawing (MovePen, DrawArc,
+ StartFilledRegion, etc). You may also need to call UpdateDisplay in your loop to flush
+ drawing to the screen without delay.
  */
 
 #include <iostream>
 #include "lib/graphics.h"
 #include "lib/extgraph.h"
+#include "lib/genlib.h"
+#include "lib/random.h"
+
+const double CIRCLE_SIZE = .05; // we need a small circle for this animation
+
+// struct stores a collection of user defined data types
+// This project relies on a Cartesian Plane, so we will need X, Y coordinates.
+struct Point {
+    double X;
+    double Y;
+};
+
+// Randomly Choose one vertex with 3 points A, B, C
+Point firstVertex; // A
+Point secondVertex; // B
+Point thirdVertex; // C
+
+Point RandomPoint(); // One random vertex will be set as the current point
+void CreateTriangle(); // We need to draw the triangle connecting A, B, C
+void SetFirstCircle(); // Draw a small filled circle around the current point
+void DrawLoop(); // Randomly choose one vertex and move the current point half of the distnace toward that vertex
+// the DrawLoop() will continue as the user clicks the mouse
 
 int main() {
     return 0;
