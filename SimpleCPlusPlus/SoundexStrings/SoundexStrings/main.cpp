a1dda7e76038b6a56da53a5db37a5c6d4c684c8b
Combatd <combatdstar@gmail.com>
2020-10-29 09:03:35 -0700

    Define #ParseChar to encode charactes into the Soundex Algorithm

--- SimpleCPlusPlus/SoundexStrings/SoundexStrings/main.cpp
+++ SimpleCPlusPlus/SoundexStrings/SoundexStrings/main.cpp
@@ -89,3 +89,23 @@ string ParseName(string name) {
     
     return code; // we will return the 4 character code here
 }
+
+// We can use conditionals to control converting alphanumeric characters into the Soundex encoded characters.
+char ParseChar(char c) {
+    if (c == 'A' || c == 'E'|| c == 'I' || c == 'O' || c == 'U' || c == 'H' || c == 'W' || c == 'Y') {
+        return '0';
+    } else if (c == 'B' || c == 'F' || c == 'P' || c == 'V') {
+        return '1';
+    } else if (c == 'C' || c == 'G' || c == 'J' || c == 'K' ||
+        c == 'Q' || c == 'S' || c == 'X' || c == 'Z') {
+        return '2';
+    } else if (c == 'D' || c == 'T') {
+        return '3';
+    } else if (c == 'M' || c == 'N') {
+        return '4';
+    } else if (c == 'L') {
+        return '5';
+    } else {
+        return '6';
+    }
+}
