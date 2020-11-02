70b3b8ddc029a10c58938f699e39b0f14ce8e98f
Combatd <combatdstar@gmail.com>
2020-11-02 00:37:02 -0800

    Create ADT.cpp to act as main

---
+++ ADTAssign/ADTAssign/ADT.cpp
@@ -0,0 +1,15 @@
+//
+//  ADT.cpp
+//  ADTAssign
+//
+//  Created by Mark Calvelo on 11/2/20.
+// Will contain the main function!
+
+#include "genlib.h"
+#include "simpio.h"
+#include "randomwriter.hpp"
+#include <iostream>
+
+int main() {
+    return 0;
+}
